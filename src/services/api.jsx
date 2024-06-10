84cc7404e4a084de516a953727de354dc411a31d
sistacuy-2022017 <sistacuy-2022017@kinal.edu.gt>
2024-06-10 15:25:56 -0600

    arreglando pedos

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://127.0.0.1:8081/almacenadora/v1',
+    baseURL: 'http://almacenadoras.vercel.app/almacenadora/v1',
     timeout: 1000
 })
 
